de930782526b5c1f2a97f83d599291d2fb25e709
Toni Leppänen <toni.leppanen@gmail.com>
2019-09-05 08:57:56 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import Datepicker from 'react-datepicker'
 
-function App() {
+import './App.css';
+import 'react-datepicker/dist/react-datepicker.css';
+
+const objToStrMap = obj => {
+  let strMap = new Map();
+  for (let k of Object.keys(obj)) {
+    strMap.set(k, obj[k]);
+  }
+  return strMap;
+}
+
+const renderSlots = slots => {
+  const res = [];
+  slots.forEach((activities, time) => res.push(
+    <tr key={time}>
+      <td>{time}</td>
+      <td>{renderActivityButtons(time, activities)}</td>
+    </tr>
+  ));
+  return res;
+}
+
+const renderActivityButtons = (time, activities) =>
+  <span key={time}>
+    {activities.includes(1) && <button>Köysilaskeutuminen</button>}
+    {activities.includes(2) && <button>Siltakeinu</button>}
+    {activities.includes(3) && <button>Kalliokiipeily</button>}
+  </span>
+
+const App = () => {
+  const [freeSlots, setFreeSlots] = useState(null);
+  const [startDate, setStartDate] = useState(new Date());
+
+  const getFreeSlots = async () => {
+    const res = await axios.get("https://gcalendar-booking.herokuapp.com/2019-09-27");
+    setFreeSlots(objToStrMap(JSON.parse(res.data)));
+  }
+
+  const onDateSelect = date => {
+    setStartDate(date);
+    getFreeSlots();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Datepicker
+        dateFormat="d.M.yyyy"
+        onChange={onDateSelect}
+        selected={startDate} />
+      <table>
+        <tbody>
+          {freeSlots && renderSlots(freeSlots)}
+        </tbody>
+      </table>
     </div>
   );
 }
